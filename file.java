69bc7a7b76f7c6162eb8292e134fe10038c99051
Anjali Battulkar <Anjali.B@quantique.in>
2025-06-16 15:23:32 +0530

    changes in both file

--- file.java
+++ file.java
@@ -3,26 +3,25 @@ import java.util.Scanner;
 
 public class file {
     static void greet(String name) {
-        System.out.println("Hi, " + name);
+        System.out.println("Hello, " + name);
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> userList = new ArrayList<>();
-
-        // Collect 5 user names
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Enter name for User " + i + ": ");
-            String name = scanner.nextLine();
-            userList.add(name);
-            greet(name);
+        try (Scanner scanner = new Scanner(System.in)) {
+            ArrayList<String> userList = new ArrayList<>();
+            
+            // Collect 5 user names
+            for (int i = 1; i <= 5; i++) {
+                System.out.print("Enter name for User " + i + ": ");
+                String name = scanner.nextLine();
+                userList.add(name);
+                greet(name);
+            }
+            
+            System.out.println("\nSummary of greeted users:");
+            for (String name : userList) {
+                System.out.println("- " + name);
+            }
         }
-
-        System.out.println("\nSummary of greeted users:");
-        for (String name : userList) {
-            System.out.println("- " + name);
-        }
-
-        scanner.close();
     }
 }
